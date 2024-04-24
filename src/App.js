071e4503d023de1cae5bcf859465d0b192e9338e
anasfaz <anaspkmmc@gmail.com>
2024-04-24 16:28:30 +0530

    new components added :head,Body

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import Header from "./components/Head";
 
 
 function App() {
   return <>
     <h1 className="font-bold text-lg text-center text-cyan-600"> hello youtube</h1>
+    
+    {/* 
+    # heading
+    #side bar
+    #body
+      >filter tag
+      >cards video card
+      >
+    
+    
+     */}
   </>;
 }
 
---
+++ src/components/Head.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <div className='w-full h-auto bg-slate-500'>Head</div>
+  )
+}
+
+export default Head
